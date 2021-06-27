beb289f467a97bbcd781942d59220307218d247d
konijntje -x <75688952+Konijntje-x@users.noreply.github.com>
2021-06-27 08:37:01 +0200

    feat(core/admin): added new configs

--- modules/__base__/rpchat/data/config.lua
+++ modules/__base__/rpchat/data/config.lua
@@ -14,6 +14,16 @@ Config = {}
 
 Config.Locale        = 'en'
 Config.Proximity     = 25.001
-Config.ProximityMode = true
-Config.OverHeadMode  = true
+Config.ProximityMode = true 
+Config.OverHeadMode  = true -- allow text over head (/me and /do)
 Config.DisableChat   = false
+-- select the chats that should be available, disable with false
+Config.Me            = true
+Config.Do            = true
+Config.Life          = true
+Config.Ooc           = true
+Config.Gooc          = true
+
+-- disble or enable discord logs for the messages
+Config.DiscordLogs   = true -- true to enable, false to disable
+Config.DiscordWebhook = 'paste your discord webhook here'
