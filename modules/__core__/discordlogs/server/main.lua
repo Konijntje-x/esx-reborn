cb0d728036fc9b44452b5164d68de657b51a5959
konijntje -x <75688952+Konijntje-x@users.noreply.github.com>
2021-06-27 09:24:30 +0200

    feat(core/discordlogs): added discord log resource

---
+++ modules/__core__/discordlogs/server/main.lua
@@ -0,0 +1,21 @@
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/esx-framework/esx-reborn
+--   This copyright should appear in every part of the project code
+
+
+function sendLogs (message,webhook)
+  if message == nil or message == '' then return FALSE end
+  PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({ content = message }), { ['Content-Type'] = 'application/json' })
+end
+
+on("esx:discordlogs:toDiscord", function(message, webhook)
+  sendLogs(message , webhook)
+end)
